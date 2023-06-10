56d0610b2b68fefe7666219b6aab2f0bc7dedb5b
Marek Tews <marek.tews@gmail.com>
2023-06-10 19:22:20 +0200

    Wariant 2 - Html5Qrcode

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <script setup>
 import { onMounted } from 'vue'
-import { Html5QrcodeScanner } from 'html5-qrcode'
+import { Html5Qrcode } from 'html5-qrcode'
 
 onMounted(() => {
-    const scanner = new Html5QrcodeScanner("reader", {
-        fps: 10,
-        qrbox: {
-            width: 350,
-            height: 350
+    Html5Qrcode.getCameras()
+    .then((devices) => {
+        if(devices && devices.length) {
+            let cameraId = devices[0].id
+            const scanner = new Html5Qrcode("reader")
+            scanner.start(
+                cameraId, 
+                {
+                    fps: 10,
+                    qrbox: {
+                        width: 350,
+                        height: 350
+                    }
+                },
+                onScanSuccess, 
+                onScanFailure
+            )
+            .catch(err => console.error(`Code scan start error = ${err}`))
         }
     })
-    scanner.render(onScanSuccess, onScanFailure)
+    .catch(err => console.error(`getCameras() error = ${err}`))
 })
 
 function onScanSuccess(decodedText, decodedResult) {
@@ -24,7 +37,7 @@ function onScanFailure(error) {
 
 <template>
     <main>
-        <div id="reader" width="600px"></div>
+        <div id="reader" width="600px" height="600px"></div>
     </main>
 </template>
 
