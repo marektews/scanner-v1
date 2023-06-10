75c4dd0678ef0f260c97793fd156826e6e807d42
Marek Tews <marek.tews@gmail.com>
2023-06-10 19:09:43 +0200

    Wariant 1 - Html5QrcodeScanner

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted } from 'vue'
+import { Html5QrcodeScanner } from 'html5-qrcode'
+
+onMounted(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+        fps: 10,
+        qrbox: {
+            width: 350,
+            height: 350
+        }
+    })
+    scanner.render(onScanSuccess, onScanFailure)
+})
+
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+    console.warn(`Code scan error = ${error}`);
+}
+</script>
+
+<template>
+    <main>
+        <div id="reader" width="600px"></div>
+    </main>
+</template>
+
+<style scoped>
+</style>
